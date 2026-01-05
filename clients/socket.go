5e19275c8259f398372859930a8427e246711b8b
dudley <dudleydogreat@gmail.com>
2026-01-04 22:13:19 -0800

    "fix path variable"

--- clients/socket.go
+++ clients/socket.go
@@ -1,4 +1,4 @@
-package main
+package clients
 
 import (
 	"fmt"
@@ -6,6 +6,11 @@ import (
 	"net"
 	"net/http"
 	"io"
+	"app/types"
+	"app/logging"
+	"encoding/json"
+	"go.uber.org/zap"
+	"strings"
 )
 
 // kubectl proxy --unix-socket=./proxy.sock --accept-hosts='^.*$'
@@ -24,20 +29,55 @@ var (
 )
 
 
-func main () {
-       resp, err := httpClient.Get("http://unix/api/v1/pods")
+func SocketResourceHandler(w http.ResponseWriter, r *http.Request) {
+    var logger = logging.NewLogger()
+    resourceType := strings.TrimPrefix(r.URL.Path, "/api/v1/resources/")
+
+    body, err := requestSocketResource(resourceType, logger)
+
+    // Hold list of results
+    results := make([]string, 0)
+
+    for _, item := range body.Items {
+        results = append(results, item.Metadata.Name)
+    }
+
+    /* Create object json response - I.e if 'services' is resource type:
+     *
+     *  {"services": [results_here]}
+     */
+    response := make(map[string]interface{})
+    response[resourceType] = results
+
+    json, err := json.Marshal(response)
+    if err != nil {
+        logger.Error("Failed marshalling json")
+    }
+
+    buffer := string(json)
+    logger.Info(buffer)
+    fmt.Fprintf(w, buffer)
+ }
+
+
+func requestSocketResource(resourceType string, logger *zap.Logger) (response types.KubeResponse, err error) {
+       resp, err := httpClient.Get("http://unix/api/v1/" + resourceType)
        if err != nil {
             panic(err)
        }
        defer resp.Body.Close()
 
-       b, err := io.ReadAll(resp.Body)
+       body, err := io.ReadAll(resp.Body)
           if err != nil {
               fmt.Printf("%s", err)
           }
 
-       fmt.Printf("%s", resp)
-       fmt.Printf("%s", b)
 
+       var data types.KubeResponse
 
+       if err := json.Unmarshal(body, &data); err != nil {
+           panic(err)
+       }
+
+       return data, err
 }
